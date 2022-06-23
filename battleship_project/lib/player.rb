17b0867720f10be3a9dad8edd3613e2b430ba9eb
TaoweiLi <104708757+TaoweiLi@users.noreply.github.com>
2022-06-22 23:47:14 -0700

    player error handling

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -2,8 +2,8 @@ class Player
   def get_move
     arr = []
     puts "enter a position with coordinates separated with a space like `4 7`"
-    input = gets.chomp
-    arr.push(input[0].to_i, input[2].to_i)
+    input = gets.chomp.split(" ")
+    arr.push(input[0].to_i, input[1].to_i)
   end
 end
 
